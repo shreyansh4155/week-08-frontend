ac1a4755e0c9eb52ed1dd344f2217b030392bccb
Shreyansh gupta <119661686+shreyansh4155@users.noreply.github.com>
2025-03-04 11:41:55 +0530

    Delete Day-03-Email directory

--- Day-03-Email/UC1.js
+++
@@ -1,8 +0,0 @@
-function validateEmail(email) {
-    const regex = /^[a-zA-Z0-9._%+-]+@bridgelabz\.co$/;
-    return regex.test(email);
-}
-
-// Test the function
-const email = "abc@bridgelabz.co";
-console.log(validateEmail(email)); 
--- Day-03-Email/UC3.js
+++
@@ -1,6 +0,0 @@
-function validateEmail(email) {
-    const regex = /^([a-zA-Z0-9._%+-]+)@bridgelabz\.([a-zA-Z0-9.-]+)\.co$/;
-    return regex.test(email);
-}
-const email = "example@bridgelabz.co";
-console.log(validateEmail(email)); 
